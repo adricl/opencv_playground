79aa618e619558d956d59c85218f0b52b60abefd
adric <adric.landman@gmail.com>
2016-04-10 17:28:36 +1000

    Added fixs to dectetion

--- test1.py
+++ test1.py
@@ -46,6 +46,7 @@ def main():
     cv2.createTrackbar('param2','image',1,255,nothing)
 
 
+
     while cv2.waitKey(1) != 27 and capWebcam.isOpened():                # until the Esc key is pressed or webcam connection is lost
         blnFrameReadSuccessfully, imgOriginal = capWebcam.read()            # read next frame
 
@@ -55,7 +56,8 @@ def main():
             break                                                           # exit while loop (which exits program)
         # end if
 
-        imgHSV = cv2.cvtColor(imgOriginal, cv2.COLOR_BGR2HSV)
+        imgHSV = imgOriginal
+        #imgHSV = cv2.cvtColor(imgOriginal, cv2.COLOR_BGR2HSV)
 
         LowH = cv2.getTrackbarPos('LowH','image')
         HighH = cv2.getTrackbarPos('HighH','image')
@@ -66,11 +68,16 @@ def main():
         LowV = cv2.getTrackbarPos('LowV','image')
         HighV = cv2.getTrackbarPos('HighV','image')
 
+        minD = cv2.getTrackbarPos('minDist','image')
+        param1 = cv2.getTrackbarPos('param1','image')
+        param2 = cv2.getTrackbarPos('param2','image')
+        dp = cv2.getTrackbarPos('dp','image')
+
 #        imgThresh = cv2.inRange(imgHSV, np.array([153, 29, 106]), np.array([184, 70, 255])) #Pink Ball
 
 #        imgThresh = cv2.inRange(imgHSV, np.array([168, 119, 63]), np.array([180, 255, 197])) #Red Ball Daylight
-        imgThresh = cv2.inRange(imgHSV, np.array([154, 255, 37]), np.array([190, 255, 255])) #Red ball night
-#        imgThresh = cv2.inRange(imgHSV, np.array([LowH, LowS, LowV]), np.array([HighH, HighS, HighV]))
+#        imgThresh = cv2.inRange(imgHSV, np.array([154, 255, 37]), np.array([190, 255, 255])) #Red ball night
+        imgThresh = cv2.inRange(imgHSV, np.array([LowH, LowS, LowV]), np.array([HighH, HighS, HighV]))
 #        imgThresh = cv2.add(imgThreshLow, imgThreshHigh)
 
 
@@ -79,30 +86,44 @@ def main():
 #	imgThresh = cv2.Th
         imgDial = cv2.dilate(imgGau, np.ones((5,5),np.uint8))
         imgErode = cv2.erode(imgDial, np.ones((5,5),np.uint8))
+        
+        str_el = cv2.getStructuringElement(cv2.MORPH_RECT, (5,5))
+        morph = cv2.morphologyEx(imgThresh, cv2.MORPH_OPEN, str_el)
+        morph = cv2.morphologyEx(morph, cv2.MORPH_CLOSE, str_el)
+
+        cont, contours, hierarchy = cv2.findContours(morph, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+        
+        print contours
+        for i in range(len(contours)):
+            (x,y), r = cv2.minEnclosingCircle(contours[i])
+            if (r >= minD):
+                cv2.circle(imgOriginal, (int(x),int(y)), int(r), (0,0,255),3)
+
 
         intRows, intColumns = imgErode.shape
 
 
 #        circles = cv2.HoughCircles(imgErode, cv2.HOUGH_GRADIENT, 5, intRows / 5)      # fill variable circles with all circles in the processed image
-        minD = cv2.getTrackbarPos('minDist','image')
-        param1 = cv2.getTrackbarPos('param1','image')
-        param2 = cv2.getTrackbarPos('param2','image')
-        dp = cv2.getTrackbarPos('dp','image')
-        
-        circles = cv2.HoughCircles(imgThresh, cv2.HOUGH_GRADIENT, dp, intRows/2)
-        if circles is not None:                     # this line is necessary to keep program from crashing on next line if no circles were found
-            print "balls " + str(circles.size)
-            for circle in circles[0]:                           # for each circle
-                x, y, radius = circle                                                                       # break out x, y, and radius
-                print "x " + str(x) + " y " + str(y) + " intCol " + str(intColumns) + " intRows " + str(intRows)
-                if ((x > 0) & (x < intRows) & (y > 0) & (y < intColumns)):
-                    print "ball position x = " + str(x) + ", y = " + str(y) + ", radius = " + str(radius)       # print ball position and radius
-                    cv2.circle(imgOriginal, (x, y), 3, (0, 255, 0), -1)           # draw small green circle at center of detected object
-                    cv2.circle(imgOriginal, (x, y), radius, (0, 0, 255), 3)                     # draw red circle around the detected object
-            # end for
-        # end if
+
+
+
+
+
+#        circles = cv2.HoughCircles(morph, cv2.HOUGH_GRADIENT, dp, intRows/2)
+#        if circles is not None:                     # this line is necessary to keep program from crashing on next line if no circles were found
+#            print "balls " + str(circles.size)
+#            for circle in circles[0]:                           # for each circle
+#                x, y, radius = circle                                                                       # break out x, y, and radius
+#                print "x " + str(x) + " y " + str(y) + " intCol " + str(intColumns) + " intRows " + str(intRows)
+#                if ((x > 0) & (x < intRows) & (y > 0) & (y < intColumns)):
+#                    print "ball position x = " + str(x) + ", y = " + str(y) + ", radius = " + str(radius)       # print ball position and radius
+#                    cv2.circle(imgOriginal, (x, y), 3, (0, 255, 0), -1)           # draw small green circle at center of detected object
+#                    cv2.circle(imgOriginal, (x, y), radius, (0, 0, 255), 3)                     # draw red circle around the detected object
+            #end for
+        #end if
 
         cv2.namedWindow("imgOriginal", cv2.WINDOW_AUTOSIZE)            # create windows, use WINDOW_AUTOSIZE for a fixed window size
+        cv2.namedWindow("imgMorph", cv2.WINDOW_AUTOSIZE)
 #        cv2.namedWindow("imgThresh", cv2.WINDOW_AUTOSIZE)           # or use WINDOW_NORMAL to allow window resizing
 #        cv2.namedWindow("imgGaus", cv2.WINDOW_AUTOSIZE)
 #        cv2.namedWindow("imgDial", cv2.WINDOW_AUTOSIZE)
@@ -123,6 +144,7 @@ def main():
         #cv2.imshow("imgGaus", imgGau)
         #cv2.imshow("imgDial", imgDial)
         cv2.imshow("imgErode", imgErode)
+        cv2.imshow("imgMorph", morph)
 
     # end while
 
