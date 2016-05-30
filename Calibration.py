18c696e35fbb69cc970e6b9f1d701a50855ff73c
adric <adric.landman@gmail.com>
2016-05-30 23:06:31 +1000

    Added Pi Camera code with pyimagesearch's library for PI and webcam code

--- Calibration.py
+++ Calibration.py
@@ -1,15 +1,11 @@
-#This code takes images and calibrates the camera based of the images
-#we expect the object to be detected in the bottom middle of the cameras view. This will calibrate on the largest object
-#in this view
+# This code takes images and calibrates the camera based of the images
+# we expect the object to be detected in the bottom middle of the cameras view. This will calibrate on the largest object
+# in this view
 
 import cv2
 import numpy as np
-import os
-import sys
 
 myDebug = False
-
-
 
 
 def nothing(x):
@@ -18,7 +14,6 @@ def nothing(x):
 
 ###################################################################################################
 def calibration(*file):
-
     cv2.namedWindow('image')
     cv2.createTrackbar('LowH', 'image', 1, 255, nothing)
     cv2.createTrackbar('HighH', 'image', 1, 255, nothing)
@@ -32,19 +27,19 @@ def calibration(*file):
     if not file:
         imgOriginal = cv2.imread('Pics/aaa.jpg', 1)
     else:
-        imgOriginal = cv2.imread(file[0],1)
+        imgOriginal = cv2.imread(file[0], 1)
 
-    imgOriginal = cv2.resize(imgOriginal, (320,240))
+    imgOriginal = cv2.resize(imgOriginal, (320, 240))
 
     result = list()
     rangeHSV = list()
-    if(myDebug):
-        rangeHSV.append((0))        
+    if (myDebug):
+        rangeHSV.append((0))
     else:
         rangeHSV = scanImage()
-    
+
     print "Checking Image \n"
-#    while(cv2.waitKey(1) != 27):
+    #    while(cv2.waitKey(1) != 27):
 
     while (len(rangeHSV) > 0):
 
@@ -61,39 +56,39 @@ def calibration(*file):
             lowHSV = np.array([LowH, LowS, LowV])
             highHSV = np.array([HighH, HighS, HighV])
 
-        #        imgThresh = cv2.inRange(imgHSV, np.array([153, 29, 106]), np.array([184, 70, 255])) #Pink Ball
+        # imgThresh = cv2.inRange(imgHSV, np.array([153, 29, 106]), np.array([184, 70, 255])) #Pink Ball
         #        imgThresh = cv2.inRange(imgHSV, np.array([168, 119, 63]), np.array([180, 255, 197])) #Red Ball Daylight
         #        imgThresh = cv2.inRange(imgHSV, np.array([154, 255, 37]), np.array([190, 255, 255])) #Red ball night
         #        imgThresh = cv2.inRange(imgHSV, np.array([78, 128, 0]), np.array([141, 255, 255]))
-       
+
         morph = filterImage(imgOriginal, lowHSV, highHSV)
-        
+
         imgCircle, count, maxRadius = circleDetectedBound(morph, imgOriginal)
 
-        #Save the best results
+        # Save the best results
         if (count > 0):
             elems = cv2.countNonZero(morph)
             result.append((imgCircle, count, maxRadius, elems, lowHSV, highHSV))
-            #print count, maxRadius, elems, lowHSV, highHSV
-        
-        if(myDebug):
+            # print count, maxRadius, elems, lowHSV, highHSV
+
+        if (myDebug):
             cv2.imshow("imgOriginal", imgCircle)  # show windows
             cv2.imshow("imgMorph", morph)
 
     # end while
     (lowHSVResult, highHSVResult) = sortResults(result)
 
-   
-    #cv2.destroyAllWindows()                     # remove windows from memory
-    #print (lowHSVResult, highHSVResult)
+    # cv2.destroyAllWindows()                     # remove windows from memory
+    # print (lowHSVResult, highHSVResult)
     return (lowHSVResult, highHSVResult)
+
 
 ###################################################################################################
 ##We optimise for low element count large radius and low count 
 def sortResults(results):
     print "Sorting Results\n"
-    results.sort(key=lambda row:row[2])
-    results.sort(key=lambda row:row[3], reverse=True)
+    results.sort(key=lambda row: row[2])
+    results.sort(key=lambda row: row[3], reverse=True)
     count = 0
     f = open('hsvVals.txt', 'w')
     f.write('CircleCount, maxRadius, elemCount, lowHSV, highHSV\n')
@@ -102,33 +97,34 @@ def sortResults(results):
         f.write(str(obj[1]) + ', ' + str(obj[2]) + ', ' + str(obj[3]) + ', ' + str(obj[4]) + ', ' + str(obj[5]) + '\n')
         cv2.imwrite(str(count) + ".png", obj[0])
 
-        count +=1
+        count += 1
     return ((results[0])[4], (results[0])[5])
 
+
 ###################################################################################################
-#Give us a range of vaules with which to determine the best HSV value set
+# Give us a range of vaules with which to determine the best HSV value set
 ########horrible function that must be rewritten 
 def scanImage():
     rangeHSV = list()
     print "Generating HSV Ranges \n"
     for i in range(1, 255, 50):
-        for j in range(1,255, 50):
-            for k in range (1,255, 50):
-                for l in range (255, 1,-50):
-                    if (i>l): break
-                    for m in range (255,1,-50):
-                        if (j>m): break
-                        for n in range (255,1,-50):
-                            if (k>n): break
-                            rangeHSV.append((np.array([i, j, k]),np.array([l, m, n])))
-        
+        for j in range(1, 255, 50):
+            for k in range(1, 255, 50):
+                for l in range(255, 1, -50):
+                    if (i > l): break
+                    for m in range(255, 1, -50):
+                        if (j > m): break
+                        for n in range(255, 1, -50):
+                            if (k > n): break
+                            rangeHSV.append((np.array([i, j, k]), np.array([l, m, n])))
 
     return rangeHSV
-    
+
+
 ###################################################################################################
 def filterImage(imgOriginal, lowHSV, highHSV):
     imgHSV = cv2.cvtColor(imgOriginal, cv2.COLOR_BGR2HSV)
-    imgThresh = cv2.inRange(imgHSV,lowHSV, highHSV)
+    imgThresh = cv2.inRange(imgHSV, lowHSV, highHSV)
 
     imgDial = cv2.dilate(imgThresh, np.ones((5, 5), np.uint8))
     imgErode = cv2.erode(imgDial, np.ones((5, 5), np.uint8))
@@ -138,30 +134,31 @@ def filterImage(imgOriginal, lowHSV, highHSV):
     morph = cv2.morphologyEx(morph, cv2.MORPH_CLOSE, str_el)
     return morph
 
+
 ###################################################################################################
 def circleDetectedBound(image, imgOriginal):
-	height, width, temp = imgOriginal.shape
-	xTop = .25 * width #Boudning box for middle bottom of the image to find ball
-	yTop = .5 * height
-	xBottom = .75 * width
-	yBottom = height
-	cont, contours, hierarchy = cv2.findContours(image, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-	#contours.sort()
-        circleImg = cv2.copyMakeBorder(imgOriginal,0,0,0,0,cv2.BORDER_REPLICATE) #copy image to display Circles
+    height, width, temp = imgOriginal.shape
+    xTop = .25 * width  # Boudning box for middle bottom of the image to find ball
+    yTop = .5 * height
+    xBottom = .75 * width
+    yBottom = height
+    cont, contours, hierarchy = cv2.findContours(image, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+    # contours.sort()
+    circleImg = cv2.copyMakeBorder(imgOriginal, 0, 0, 0, 0, cv2.BORDER_REPLICATE)  # copy image to display Circles
 
-        circleCount = 0
-        maxRadius = 0
-        cv2.rectangle(circleImg, (int(xTop),int(yTop)), (int(xBottom), int(yBottom)), (0,255,0), 3)
-	for i in range(len(contours)):
-		(x, y), r = cv2.minEnclosingCircle(contours[i])
-		if (xTop <= x and x <= xBottom and yTop <= y and y <= yBottom): 
-		    cv2.circle(circleImg, (int(x), int(y)), int(r), (0, 0, 255), 3)
-                    circleCount +=1
-                    maxRadius = max(maxRadius, r)
-                else:
-                    return circleImg, 0, 0
-		#intRows, intColumns = imgErode.shape
-	return circleImg, circleCount, maxRadius
+    circleCount = 0
+    maxRadius = 0
+    cv2.rectangle(circleImg, (int(xTop), int(yTop)), (int(xBottom), int(yBottom)), (0, 255, 0), 3)
+    for i in range(len(contours)):
+        (x, y), r = cv2.minEnclosingCircle(contours[i])
+        if (xTop <= x and x <= xBottom and yTop <= y and y <= yBottom):
+            cv2.circle(circleImg, (int(x), int(y)), int(r), (0, 0, 255), 3)
+            circleCount += 1
+            maxRadius = max(maxRadius, r)
+        else:
+            return circleImg, 0, 0
+            # intRows, intColumns = imgErode.shape
+    return circleImg, circleCount, maxRadius
 
 
 if __name__ == "__main__":
--- detectBall.py
+++ detectBall.py
@@ -1,47 +1,35 @@
 import cv2
-import numpy as np
-import sys
-import os
-import io
-import datetime
 import Calibration
+from imutils.video import VideoStream
+import datetime
+import time
 
 IS_PI = False
-
-if (IS_PI):
-    import picamera
 
 CAM_LOCATION = 0
 
 def main():
-
     calib = []
-    for i in range(1,2):
-
+    for i in range(1, 2):
         filename = takePicture()
-        calibRes = Calibration.calibration(filename) #calibrate camera many times to average the calibration
+        calibRes = Calibration.calibration(filename)  # calibrate camera many times to average the calibration
         calib.append(calibRes)
 
     capWebcam = openCamera()
 
-    while capWebcam.isOpened():
-        blnFrameReadSuccessfully, imgOriginal = capWebcam.read()  # read next frame
+    while True:
+        imgOriginal = capWebcam.read()  # read next frame
 
-        if not blnFrameReadSuccessfully or imgOriginal is None:  # if frame was not read successfully
-            print "error: frame not read from webcam\n"  # print error message to std out
-            os.system("pause")  # pause until user presses a key so user can see error message
-            break
-
-            #get location of ball
-        filteredImg = Calibration.filterImage(imgOriginal,(calib[0])[0], (calib[0])[1])
+        # get location of ball
+        filteredImg = Calibration.filterImage(imgOriginal, (calib[0])[0], (calib[0])[1])
 
         print location(filteredImg)
 
-    #endWhile
+        # endWhile
 
 
 def location(filteredImg):
-    maxRadius, xMax, yMax = (0, 0,0)
+    maxRadius, xMax, yMax = (0, 0, 0)
     cont, contours, hierarchy = cv2.findContours(filteredImg, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
 
     for i in range(len(contours)):
@@ -53,38 +41,34 @@ def location(filteredImg):
 
     return (xMax, yMax, maxRadius)
 
+
 ###################################################################################################
 
 def openCamera():
-
     if (IS_PI):
-        stream = io.BytesIO()
-        with picamera.PiCamera() as camera:
-            camera.framerate = 10
-            camera.start_preview()
-            camera.capture(stream, format='jpeg')
+        vs = VideoStream(usePiCamera=1).start()
 
-            data = np.fromstring(stream.getvalue(), dtype=np.uint8)
-            cam = image = cv2.imdecode(data, 1)
     else:
-        cam = capWebcam = cv2.VideoCapture(CAM_LOCATION)
+        vs = VideoStream()
 
-    if capWebcam.isOpened() == False:                           # check if VideoCapture object was associated to webcam successfully
-        print "error: capWebcam not accessed successfully\n\n"          # if not, print error message to std out
-        sys.exit(-1)                                       # pause until user presses a key so user can see error message
+    time.sleep(2.0)
 
-    return cam
+    return vs
+
+
 ###################################################################################################
 def takePicture():
     filename = 'Pics/' + str(datetime.datetime.now()) + '.png'
 
     cam = openCamera()
-    s, im = cam.read()
+    im = cam.read()
     cv2.imwrite(filename, im)
-    cam.release()
+    cam.stop()
     return filename
+
+
 ###################################################################################################
 
 if __name__ == "__main__":
-    #main(sys.argv)
+    # main(sys.argv)
     main()
